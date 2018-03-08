ab51abaa1e103c4989fc1291fddf5ea1f559dfc8
nPeritore <nils;peritore@epitech.eu>
2018-03-08 02:52:04 +0100

    Partie contacts - optimiations

--- cf/src/component/contacts.js
+++ cf/src/component/contacts.js
@@ -10,40 +10,34 @@ export default class myValues extends PureComponent{
             letterSpacing: "-0.02em !important",
             marginTop:"30px",
         };
-        return <Container>
-            <Row style={{paddingTop: "100px", color: "white"}}>
-                <Col>
-                    <Jumbotron style={{background: "none"}}>
-                        <h1 className="display-3">La Centrale Fitness reste à votre écoute !</h1>
-                        <h4 style={h4Css}>Afin de vous fournir les meilleurs réponses à vos questions la Centrale Fitness met à votre disposition plusieurs services.</h4>
-                        <hr className="my-2" />
-                        <Row>
-                            <Col>
-                                <Row>
-                                    <Col md={3}>
-                                        <Img width={"90%"} src={require("../img/contacts/004-envelope.png")} />
-                                    </Col>
-                                    <Col>
-                                        <h5>Par mail:</h5>
-                                        <h5>centrale@fitness.com</h5>
-                                    </Col>
-                                </Row>
+        return (
+            <Jumbotron  style={{paddingTop: "100px", color: "white", background: "none"}}>
+                <h1 className="display-3">La Centrale Fitness reste à votre écoute !</h1>
+                <h4 style={h4Css}>Afin de vous fournir les meilleurs réponses à vos questions la Centrale Fitness met à votre disposition plusieurs services.</h4>
+                <hr className="my-2" />
+                <Container>
+                    <Row style={{paddingTop: "100px"}}>
+                        <Col xl={6} lg={6} md={6} sm={12}  xs={12} offset={{sm: 0, xs: 0, md: 0, xl: 0, lg: 0}}>
+                            <Col >
+                                <Img width={"50%"} src={require("../img/contacts/004-envelope.png")} />
                             </Col>
                             <Col>
-                                <Row>
-                                    <Col md={3}>
-                                        <Img width={"90%"} src={require("../img/contacts/001-phone-number.png")} />
-                                    </Col>
-                                    <Col>
-                                        <h5>Par téléphone:</h5>
-                                        <h5>+000-000-000</h5>
-                                    </Col>
-                                </Row>
+                                <h5>Par mail:</h5>
+                                <h5>centrale@fitness.com</h5>
                             </Col>
-                        </Row>
-                    </Jumbotron>
-                </Col>
-            </Row>
-        </Container>
+                        </Col>
+                        <Col xl={6} lg={6} md={6} sm={12}  xs={12} offset={{sm: 0, xs: 0, md: 0, xl: 0, lg: 0}}>
+                            <Col >
+                                <Img width={"50%"} src={require("../img/contacts/001-phone-number.png")} />
+                            </Col>
+                            <Col>
+                                <h5>Par téléphone:</h5>
+                                <h5>+000-000-000</h5>
+                            </Col>
+                        </Col>
+                    </Row>
+                </Container>
+            </Jumbotron>
+        )
     }
 }
