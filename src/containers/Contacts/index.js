17b2bd2a52d27d6d475142df1321a5f17b660103
Nils <nils.peritore@epitech.eu>
2018-04-15 17:34:54 +0200

    New webpack version / New introduction / New footer

--- src/containers/Contacts/index.js
+++ src/containers/Contacts/index.js
@@ -1,69 +1,118 @@
 import React, { PureComponent } from 'react';
 import { Container, Row, Col } from 'react-grid-system';
-import { Jumbotron, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import Img from 'react-image';
 import Letter from '../../assets/images/contacts/004-envelope.png';
 import Phone from '../../assets/images/contacts/001-phone-number.png';
 
-const h4Css = {
-  color: 'white',
-  letterSpacing: '-0.02em !important',
-  marginTop: '30px',
-  textAlign: 'center'
-};
 export default class myValues extends PureComponent {
   render() {
     return (
-      <Jumbotron style={ { paddingTop: '100px', color: 'white', backgroundImage: 'linear-gradient(to top, #00c6fb 0%, #005bea 100%)' } }>
-        <h1 style={ { textAlign: 'center' } } className="display-3">La Centrale Fitness reste à votre écoute !</h1>
-        <Form>
-          <FormGroup>
-            <Label for="Email">Email</Label>
-            <Input type="email" name="email" id="Email" placeholder="Veuillez entrer votre adresse email" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="firstName">Prénom</Label>
-            <Input type="name" name="password" id="firstName" placeholder="password placeholder" />
-
-            <Label for="lastName">Nom</Label>
-            <Input type="name" name="password" id="lastName" placeholder="password placeholder" />
-          </FormGroup>
-          <Button>Submit</Button>
-        </Form>
-        <h4 style={ h4Css }>Afin de vous fournir les meilleurs réponses à vos questions la Centrale Fitness met à votre
-          disposition plusieurs services.</h4>
-        <hr className="my-2" />
-        <Container>
-          <Row style={ { paddingTop: '50px', textAlign: 'center' } }>
-            <Col xl={ 6 } lg={ 6 } md={ 6 } sm={ 12 } xs={ 12 } offset={ { sm: 0, xs: 0, md: 0, xl: 0, lg: 0 } }>
+      <Container
+        fluid
+        style={ {
+          padding: '90px 0 30px 0',
+          backgroundImage: 'linear-gradient(to left bottom, #FF5889, #FFBD68)'
+        } }
+      >
+        <Row>
+          <Col style={ { textAlign: 'center', paddingBottom: '30px', color: 'white' } }>
+            <h2>La Centrale Fitness reste à votre écoute !</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col style={ { textAlign: 'center', paddingBottom: '30px', color: 'white' } }>
+            <h3>Restez informer des nouveautés de Centrale
+              Fitness
+              avec notre service de Newsletter</h3>
+            <hr className="my-2" />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Row>
               <Col>
-                <Img width={ '35%' } src={ Letter } />
+                <Form style={ { maxWidth: '700px', margin: 'auto', paddingBottom: '90px' } }>
+                  <Row>
+                    <Col style={ { color: 'white' } }>
+                      <FormGroup>
+                        <Label for="Email">Email</Label>
+                        <Input type="email" name="email" id="Email" placeholder="Veuillez entrer votre adresse email" />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <FormGroup>
+                    <Row>
+                      <Col
+                        xl={ 6 }
+                        lg={ 6 }
+                        md={ 6 }
+                        sm={ 12 }
+                        xs={ 12 }
+                        style={ { color: 'white' } }
+                      >
+                        <Label for="firstName">Prénom</Label>
+                        <Input type="text" name="firstNameData" id="firstName" placeholder="Votre prénom" />
+                      </Col>
+                      <Col
+                        xl={ 6 }
+                        lg={ 6 }
+                        md={ 6 }
+                        sm={ 12 }
+                        xs={ 12 }
+                        style={ { color: 'white' } }
+                      >
+                        <Label for="lastName">Nom</Label>
+                        <Input type="text" name="lastNameData" id="lastName" placeholder="Votre nom de famille" />
+                      </Col>
+                    </Row>
+                  </FormGroup>
+                  <Button>Submit</Button>
+                </Form>
               </Col>
-              <Col>
-                <h5 style={ { marginTop: '15px' } }>Par mail:</h5>
-                <h5 style={ { marginBottom: '100px' } }>centrale@fitness.com</h5>
+            </Row>
+            <Row>
+              <Col style={ { textAlign: 'center', color: 'white' } }>
+                <h3>Afin de vous fournir les meilleurs réponses à vos questions la Centrale Fitness met
+                  à
+                  votre
+                  disposition plusieurs services.</h3>
               </Col>
-            </Col>
-            <Col
-              style={ { textAlign: 'center' } }
-              xl={ 6 }
-              lg={ 6 }
-              md={ 6 }
-              sm={ 12 }
-              xs={ 12 }
-              offset={ { sm: 0, xs: 0, md: 0, xl: 0, lg: 0 } }
-            >
-              <Col>
-                <Img width={ '35%' } src={ Phone } />
+            </Row>
+            <hr className="my-2" />
+            <Row style={ { paddingTop: '50px', textAlign: 'center' } }>
+              <Col xl={ 6 } lg={ 6 } md={ 6 } sm={ 12 } xs={ 12 } offset={ { sm: 0, xs: 0, md: 0, xl: 0, lg: 0 } }>
+                <Col>
+                  <Img width={ '30%' } src={ Letter } />
+                </Col>
+                <Col style={ { color: 'white' } }>
+                  <h4 style={ { marginTop: '15px' } }>Par mail:</h4>
+                  <h4 style={ { marginBottom: '100px' } }>centrale<span
+                    style={ { fontFamily: 'Circular,Helvetica,Arial,sans-serif' } }
+                  >@</span>fitness.com</h4>
+                </Col>
               </Col>
-              <Col>
-                <h5 style={ { marginTop: '15px' } }>Par téléphone:</h5>
-                <h5>+000-000-000</h5>
+              <Col
+                style={ { textAlign: 'center' } }
+                xl={ 6 }
+                lg={ 6 }
+                md={ 6 }
+                sm={ 12 }
+                xs={ 12 }
+                offset={ { sm: 0, xs: 0, md: 0, xl: 0, lg: 0 } }
+              >
+                <Col>
+                  <Img width={ '30%' } src={ Phone } />
+                </Col>
+                <Col style={ { color: 'white' } }>
+                  <h4 style={ { margin: '15px 0 15px 0' } }>Par téléphone:</h4>
+                  <h4 style={ { fontFamily: 'Circular,Helvetica,Arial,sans-serif' } }>+000-000-000</h4>
+                </Col>
               </Col>
-            </Col>
-          </Row>
-        </Container>
-      </Jumbotron>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
